fd1c4956ff44785e8ded1f15c048f3719c886387
Deeptiwani <Deeptiwani@deeptiIntern.VERINITE1.local>
2024-07-18 14:53:00 +0530

    Initial Commit

--- src/main/java/com/bankingapplication/BankingApplication.java
+++ src/main/java/com/bankingapplication/BankingApplication.java
@@ -2,12 +2,14 @@ package com.bankingapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class BankingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BankingApplication.class, args);
+    }
 }
